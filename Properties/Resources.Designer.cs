e4677acef65ad2f67e0c1e841f6c00ed9fdd590b
Sebastian Dorado Botero <ceceptrabajos123@gmail.com>
2023-04-28 23:55:04 -0500

    Main View

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -139,5 +139,15 @@ namespace Supermarket_mvp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap sell {
+            get {
+                object obj = ResourceManager.GetObject("sell", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
